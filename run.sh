0f60e9de092e147ea300458ef6688d1fc72b36f0
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-25 08:39:28 +0530

    RoboShop Ansible code

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ -z "$component" -o -z "$env" -o -z "$ssh_password" ]; then
   exit 1
 fi
 
-ansible-playbook -i $component-$env.rdevopsb79.online, -e ansible_user=ec2-user -e ansible_password=$ssh_password -e component=$component main.yml
+ansible-playbook -i $component-$env.rdevopsb79.online, -e env=$env -e ansible_user=ec2-user -e ansible_password=$ssh_password -e component=$component main.yml
