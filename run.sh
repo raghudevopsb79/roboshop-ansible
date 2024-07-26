e4e302ea977e530359d498d5a2d51b2407c058fb
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-26 08:10:27 +0530

    RoboShop Ansible code

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-if [ -z "$component" -o -z "$env" -o -z "$ssh_password" ]; then
+if [ -z "$component" -o -z "$env" -o -z "$ssh_password" -o -z "$vault_token" ]; then
   echo Input Missing
   exit 1
 fi
 
-ansible-playbook -i $component-$env.rdevopsb79.online, -e env=$env -e ansible_user=ec2-user -e ansible_password=$ssh_password -e component=$component main.yml
+ansible-playbook -i $component-$env.rdevopsb79.online, -e env=$env -e ansible_user=ec2-user -e ansible_password=$ssh_password -e component=$component -e vault_token=$vault_token main.yml
